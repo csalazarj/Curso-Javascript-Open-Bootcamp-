94598412a3c9642561c53203704818e5ef37db47
csalazarj <csalazar.red@gmail.com>
2023-01-08 18:30:24 -0500

    funcion factory

--- Sesion 11 - POO/index.js
+++ Sesion 11 - POO/index.js
@@ -7,4 +7,21 @@ const persona = {
   },
 };
 
-persona.saludo()
+persona.saludo();
+// Funcion factory
+const creaPersona = (nombre, edad, isDeveloper) => {
+  return {
+    nombre,
+    edad,
+    isDeveloper,
+    saludo: function () {
+      console.log("Hello");
+    },
+  };
+};
+
+const persona2 = creaPersona("juan", 23, true);
+const persona3 = creaPersona("carlos", 24, false);
+
+console.log("persona 2", persona2);
+console.log("persona 3", persona3);
