e618d8fb859b5fa5b07aa8a5e684f884a024bdc4
csalazarj <csalazar.red@gmail.com>
2023-01-10 19:04:47 -0500

    eslint inline

---
+++ Sesion 13 - ESLint/eslint-npm/index.js
@@ -0,0 +1,17 @@
+const nombre = "cris"
+
+/* eslint-disable*/
+const p2 = 'Maria'
+
+/* eslint-enable*/
+
+// En esta linea quiero tener comillas simples (quiero que me desactives la regla de las comillas dobles)
+const nuevoString = 'Nuevo string' // eslint-disable-line
+
+/* eslint-disable-next-line indent */
+    const string2 = "asasdasdasd"
+console.log("lasdadasd")
+console.log("lasdadasd")
+const string3 = "asasdasdasd"
+
+const string4 = "asasdasdasd"
