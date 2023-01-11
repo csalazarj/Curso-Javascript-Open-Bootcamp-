2fb4ab05114e6ad7d8b62d62c0b33cc77406396b
csalazarj <csalazar.red@gmail.com>
2023-01-10 19:46:31 -0500

    ejercicio 13: eslint

---
+++ Sesion 13 - ESLint/ejercicio13/index.js
@@ -0,0 +1,18 @@
+const nombre = "Cristhian"
+const apellido = "Salazar"
+
+const estudiante = `${nombre} ${apellido}`
+
+const estudianteMayus = estudiante.toUpperCase()
+
+const estudianteMinus = estudiante.toLowerCase()
+
+const len = estudiante.length
+
+const primera = nombre[0]
+
+const ultima = apellido[apellido.length - 1]
+
+const sin_espacios = estudiante.trim()
+
+const boo = estudiante.includes(nombre)
