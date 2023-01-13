e2064f35711161e9c22e99b95c74502072f81066
csalazarj <csalazar.red@gmail.com>
2023-01-13 13:09:06 -0500

    Geolocalizacion del navegador

--- Sesion 17 - Maps/index.js
+++ Sesion 17 - Maps/index.js
@@ -1,3 +1,5 @@
+let marker, map;
+
 function initMap() {
   // console.log("Inicializando...")
   const posicion = {
@@ -5,14 +7,43 @@ function initMap() {
     lng: 131.044,
   };
 
-  const map = new google.maps.Map(document.getElementById("map"), {
+  map = new google.maps.Map(document.getElementById("map"), {
     zoom: 4,
     center: posicion,
   });
 
-  const marker = new google.maps.Marker({
+  marker = new google.maps.Marker({
     position: posicion,
     map,
     title: "Posición Inicial",
   });
+
+  // marker.setPosition({lat, lng})
+  // Obtener la Geolocalización
+  geoPosiciona();
+}
+
+function geoPosiciona() {
+  if (navigator.geolocation) {
+    const geoLoc = navigator.geolocation;
+    const options = { timeout: 60000 };
+    const watchPos = geoLoc.watchPosition(centraMapa, onError, options);
+  } else {
+    alert("Tu navegador no admite geolocalización");
+  }
+}
+
+function centraMapa(position) {
+  const nuevaPos = {
+    lat: position.coords.latitude,
+    lng: position.coords.longitude,
+  };
+  console.log(nuevaPos);
+  marker.setPosition(nuevaPos);
+  map.setCenter(nuevaPos);
+}
+
+function onError(error) {
+  console.log("Se ha producido un error y se ha gestionado");
+  console.error(error);
 }
